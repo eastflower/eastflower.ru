78c212b1ec457ff54dca64846df05243a35c113a
Ilya N. Zykin <ilya.zykin@toptal.com>
2018-03-24 19:22:54 +0300

    Improve paths

--- config/app_theme/configs.rb
+++ config/app_theme/configs.rb
@@ -5,11 +5,12 @@
 Config::Integration::Rails::Railtie.preload
 app = Rails.application
 
-Dir[
-  "#{ app.config.root }/config/_CONFIGS/#{ ::Rails.env }/settings/*.yml",
-  "#{ app.config.root }/config/ENV/#{ ::Rails.env }/settings/*.yml",
-  "#{ app.config.root }/config/settings/*.yml"
-].each do |settings_file|
+puts path_1 = "#{ app.config.root }/config/_CONFIGS/#{ ::Rails.env }/settings/*.yml"
+puts path_2 = "#{ app.config.root }/config/ENV/#{ ::Rails.env }/settings/*.yml"
+puts path_3 = "#{ app.config.root }/config/settings/*.yml"
+puts '=*'*20
+
+Dir[ path_1, path_2, path_3 ].each do |settings_file|
   Settings.add_source!(settings_file)
   puts 'Settings: LOAD FILE => `' + settings_file + '`'
 end
