12f1558438686c4260b8366ca51c7fb274220ac8
Ilya N. Zykin <ilya.zykin@toptal.com>
2018-03-24 19:00:40 +0300

    Add a path with config settings

--- config/app_theme/configs.rb
+++ config/app_theme/configs.rb
@@ -7,7 +7,8 @@ app = Rails.application
 
 Dir[
   "#{ app.config.root }/config/_CONFIGS/#{ ::Rails.env }/settings/*.yml",
-  "#{ app.config.root }/config/settings/*.yml",
+  "#{ app.config.root }/ENV/#{ ::Rails.env }/settings/*.yml",
+  "#{ app.config.root }/config/settings/*.yml"
 ].each do |settings_file|
   Settings.add_source!(settings_file)
   puts 'Settings: LOAD FILE => `' + settings_file + '`'
