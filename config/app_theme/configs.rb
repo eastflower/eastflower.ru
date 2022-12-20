9eaaf5c5c8ec992c2d22c09693a3a9adb8485b1b
Ilya N. Zykin <zykin-ilya@ya.ru>
2022-12-20 14:58:59 +0300

    update 2023

--- config/app_theme/configs.rb
+++ config/app_theme/configs.rb
@@ -5,12 +5,11 @@
 Config::Integration::Rails::Railtie.preload
 app = Rails.application
 
-puts path_1 = "#{ app.config.root }/config/_CONFIGS/#{ ::Rails.env }/settings/*.yml"
-puts path_2 = "#{ app.config.root }/config/ENV/#{ ::Rails.env }/settings/*.yml"
-puts path_3 = "#{ app.config.root }/config/settings/*.yml"
+puts path_1 = "#{ app.config.root }/config/ENV/#{ ::Rails.env }/settings/*.yml"
+puts path_2 = "#{ app.config.root }/config/settings/*.yml"
 puts '=*'*20
 
-Dir[ path_1, path_2, path_3 ].each do |settings_file|
+Dir[ path_1, path_2 ].each do |settings_file|
   Settings.add_source!(settings_file)
   puts 'Settings: LOAD FILE => `' + settings_file + '`'
 end
